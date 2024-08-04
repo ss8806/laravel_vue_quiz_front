369b90d027ea21f010fb4f619a63a1469c88b603
ss <>
2024-08-04 15:44:44 +0900

    scss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free-webfonts'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css'
+import '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css'
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import '@/assets/scss/style.scss';` },
+    }
+  },
 })
